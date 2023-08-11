2cdb7f65452c66720ea3378ae991fd166575ba6d
Saajonsayrest <sajonshrestha5@gmail.com>
2023-08-11 13:13:07 +0545

    testiing

--- lib/main.dart
+++ lib/main.dart
@@ -13,7 +13,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.teal),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.yellow),
         useMaterial3: true,
       ),
       home: const MyHomePage(title: 'Portfolio App'),
